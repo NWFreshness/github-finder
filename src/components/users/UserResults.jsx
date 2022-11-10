589c9169286baae362920006491d5cc3856285d4
Tyler Mayfield <tyler.mayfield23@gmail.com>
2022-11-10 12:16:38 -0800

    fixing .env

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -9,11 +9,12 @@ function UserResults() {
     fetchUsers();
   }, [])
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-      }
-    })
+    // const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+    //   headers: {
+    //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+    //   }
+    // })
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
 
     const data = await response.json();
     setUsers(data);
